24849999fbf343826826e21d1780a7e9b97714b3
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-05-05 20:31:53 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,26 @@
+
+public class Solution {
+
+    public int numberOfCategories(int numberOfElements, CategoryHandler categoryHandler) {
+
+        int numberOfCategories = numberOfElements;
+
+        for (int first = 0; first < numberOfElements; ++first) {
+            for (int second = first + 1; second < numberOfElements; ++second) {
+                if (categoryHandler.haveSameCategory(first, second)) {
+                    --numberOfCategories;
+                    break;
+                }
+            }
+        }
+        return numberOfCategories;
+    }
+}
+
+// class 'CategoryHandler' is in-built in the solution file on leetcode.com
+/**
+ * Definition for a category handler. 
+ * class CategoryHandler { 
+ * public CategoryHandler(int[] categories); 
+ * public boolean haveSameCategory(int a, int b); };
+ */
